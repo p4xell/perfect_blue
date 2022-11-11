693f87fdbdd0df5aa1578daa43e570324c1b55f6
Roman <d4vegame@gmail.com>
2022-11-11 08:50:37 +0300

    templates

--- perfect_blue/flowers/views.py
+++ perfect_blue/flowers/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import *
+
+menu = [
+    {'title': 'Главная', 'url': 'main'},
+    {'title': 'Хит продаж', 'url': 'bestseller'},
+    {'title': 'Корзина', 'ulr': 'basket'},
+    {'title': 'Профиль', 'url': 'profile'}
+]
+
+cats = Categories.objects.all()
 
 
-def test(request):
-    return render(request, 'flowers/base.html')
+class AllFlowers(ListView):
+    model = Flowers
+    template_name = 'flowers/base.html'
+    context_object_name = 'flowers'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['categories'] = cats
+        return context
--- perfect_blue/perfect_blue/settings.py
+++ perfect_blue/perfect_blue/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -111,9 +111,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'stastic')
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
